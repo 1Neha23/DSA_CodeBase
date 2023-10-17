181b7ca66d6984697ed162b5753d30249d28b3e3
Neha <nehamyadav22@gmail.com>
2023-10-17 16:19:33 +0530

    Prime number program added

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+public class PrimeNumber {
+    
+    static boolean isPrime(int n)
+    {
+        for(int i=2; i< n;i++ )
+        {
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+    
+    static int checkPrime(int num)
+    {
+        int count =0;
+        for(int i=2; i< num ;i++)
+        {
+            if(isPrime(i))
+            {
+                count++;
+                System.out.println(i);
+            }
+        }
+         
+        return count;
+       
+    }
+    public static void main(String args[])
+    {
+      int num =20;
+     int n = checkPrime(num);
+     System.out.println("count is" + n);
+      
+    }
+}
