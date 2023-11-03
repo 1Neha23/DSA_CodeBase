e65fc76a7a1444434279fe370518d20aff5f4cf6
Neha <nehamyadav22@gmail.com>
2023-11-03 21:45:38 +0530

    toLowecase

---
+++ TolowerCase.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class TolowerCase {
+    
+    
+   
+    public static String toLowerCase(String s) {
+        
+       
+         return s.toLowerCase();
+        
+        
+        
+    }
+
+    public static void main(String args[]) {
+     String s= "Nehaaa";
+      toLowerCase(s);
+     System.out.print( toLowerCase(s));
+    }
+}
