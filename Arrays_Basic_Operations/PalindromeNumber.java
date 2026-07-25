710842bb6ed733c36f7782fa34903c670f138a7a
Neha Yadav <nehayadav@Nehas-MacBook-Air.local>
2026-07-25 22:47:35 +0530

    NEHA | Palindrome Check code added

--- Arrays_Basic_Operations/PalindromeNumber.java
+++ Arrays_Basic_Operations/PalindromeNumber.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class PalindromeNumber
 {
-    public static boolean checkPalindrome_Any_Type(Object obj)
+    public static String checkPalindrome_Any_Type(Object obj)
     {
         if(obj instanceof String)
         {
@@ -15,7 +15,7 @@ public class PalindromeNumber
         else
         {
             System.out.println("Unsupported type. Please provide String or Integer type.");
-            return false;
+            return "Object type value is not Palindrome";
 
         }
 
@@ -27,27 +27,82 @@ public class PalindromeNumber
         String reversed = sb.reverse().toString();
         if(reversed.equalsIgnoreCase(str))
         {
-            return str;
+            return "string is palindrome";
+        }
+        else
+        {
+            return "string is not palindrome";
         }
     
          
     }
 
-    public static int checkNumberPalindrome(int num)
+    public static String checkNumberPalindrome(Number num)
     {
-        int rem=0;
-        while(num !=0)
+        long original = num.longValue();
+        long rem=0;
+        long rev=0;
+        while(original !=0)
         {
-        rem= num %10;
-        int rev = rev * 10 + rem;
-        num = num / 10;
+        rem= original % 10;
+        rev = rev * 10 + rem;
+        original = original / 10;
         }
-        return rev;
+        if(rev==num.longValue())
+        {
+            return "number is palindrome";
+        }
+        else
+        {
+            return "number is not palindrome";
+        }
     }
 
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+        System.out.println("Pick any. one option");
+        System.out.println("1. checkNumberPalindrome");
+        System.out.println("2 .checkStringPalindrome");
+        System.out.println("3. checkPalindrome_Any_Type");
+        System.out.println("4. Exit");
+            int option = sc.nextInt();
+            switch(option)
+            {
+                case 1:
+                    {
+                    System.out.println("enter number");
+                    Number number = sc.nextInt();
+                    String result = checkNumberPalindrome(number);
+                    System.out.println(result);
+                    break;
+                    }
+                case 2:
+                    {
+                    System.out.println("enter String");
+                    String str  = sc.next();
+                    String result = checkStringPalindrome(str);
+                    System.out.println(result);
+                    break;
+                    }   
+                case 3:
+                    {
+                    System.out.println("enter value which take as Object type");
+                    Object value = sc.next();
+                    String result = checkPalindrome_Any_Type(value);
+                    System.out.println(result);
+                    break;
+                    }   
+                case 4:
+                    {
+                        System.out.println("Exiting");
+                        System.exit(0);
+                        break;
+                    } 
+            }
+        }
 
 
     }
